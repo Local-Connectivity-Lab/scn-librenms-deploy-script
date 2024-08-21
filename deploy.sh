fc409553fbcbdd830d0c6a295f1bc117f9d1424b
Mark Nash <abacefabmicroblast@hotmail.com>
2024-08-21 16:39:58 -0700

    Added check for docker compose and error if we are not on debian or ubuntu

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,8 @@
-set -e
-
-if ! [[ $(which docker) && $(docker --version) ]]; then
-  echo "Docker is not installed. Please install docker."
-  exit 1
-fi
-
-if ! [[ $(which unzip) ]]; then
-  echo "unzip is not installed. Please install it"
-  exit 1
-fi
-
-cd librenms_image
-sudo docker build . -t scn-librenms
-cd ..
-
-if [ -f "librenms.sql" ]; then
-  mkdir tmp
-  cp db_image_with_backup/Dockerfile tmp
-  cp librenms.sql tmp
-  cd tmp
-  sudo docker build . -t scn_mariadb_librenms
-  cd ..
-  rm -r tmp
+if grep -q "Ubuntu\|Debian" /etc/os-release; then
+  echo "Running on Ubuntu or Debian"
+  source deploy_debian_or_ubuntu.sh
 else
-  cd db_image
-  sudo docker build . -t scn_mariadb_librenms
-  cd ..
-fi
-
-sudo docker compose -f compose/compose.yml up -d
-
-sleep 5
-
-if [ -f "rrd.zip" ]; then
-  cd compose/librenms
-  cp ../../rrd.zip .
-  unzip rrd.zip
-  rm rrd.zip
-  cd ../..
+  echo "Not running on Ubuntu or Debian. Not sure how to install"
+  exit 1;
 fi
 
---
+++ deploy_debian_or_ubuntu.sh
@@ -0,0 +1,47 @@
+if ! [[ $(which docker) && $(docker --version) ]]; then
+  echo "Docker is not installed. Please install docker."
+  exit 1
+fi
+
+if ! [[ $(which unzip) ]]; then
+  echo "unzip is not installed. Please install it"
+  exit 1
+fi
+
+docker compose version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "docker compose not installed. Please install the docker compose plugin"
+  exit 1
+fi
+
+set -e
+
+cd librenms_image
+sudo docker build . -t scn-librenms
+cd ..
+
+if [ -f "librenms.sql" ]; then
+  mkdir tmp
+  cp db_image_with_backup/Dockerfile tmp
+  cp librenms.sql tmp
+  cd tmp
+  sudo docker build . -t scn_mariadb_librenms
+  cd ..
+  rm -r tmp
+else
+  cd db_image
+  sudo docker build . -t scn_mariadb_librenms
+  cd ..
+fi
+
+sudo docker compose -f compose/compose.yml up -d
+
+sleep 5
+
+if [ -f "rrd.zip" ]; then
+  cd compose/librenms
+  cp ../../rrd.zip .
+  unzip rrd.zip
+  rm rrd.zip
+  cd ../..
+fi
